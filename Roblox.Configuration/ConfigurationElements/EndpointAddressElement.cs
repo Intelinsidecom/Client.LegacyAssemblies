b92ffb95237972b5e6e87a08b2da822ce516e436
Intel Inside <145130084+Intelinsidecom@users.noreply.github.com>
2026-08-20 21:30:50 +0300

    initial commit
    i added the minimal version here i found

---
+++ Roblox.Configuration/ConfigurationElements/EndpointAddressElement.cs
@@ -0,0 +1,43 @@
+﻿using System.Configuration;
+
+namespace Roblox.Configuration
+{
+    public class EndpointAddressElement : ConfigurationElement
+	{
+		static EndpointAddressElement()
+		{
+            nameProperty = new ConfigurationProperty("endpointConfigurationName", typeof(string), null, ConfigurationPropertyOptions.IsRequired);
+            properties.Add(nameProperty);
+			connectionStringProperty = new ConfigurationProperty("connectionString", typeof(string), null, ConfigurationPropertyOptions.IsRequired);
+			properties.Add(connectionStringProperty);
+		}
+
+		public string EndpointConfigurationName
+        {
+            get
+            {
+                return (string)base[nameProperty];
+            }
+            set
+            {
+                base[nameProperty] = value;
+            }
+        }
+        public string ConnectionString
+        {
+            get
+            {
+                return (string)base[connectionStringProperty];
+            }
+            set
+            {
+                base[connectionStringProperty] = value;
+            }
+        }
+        protected override ConfigurationPropertyCollection Properties => properties;
+
+        private static readonly ConfigurationPropertyCollection properties = new ConfigurationPropertyCollection();
+		private static readonly ConfigurationProperty nameProperty;
+		private static readonly ConfigurationProperty connectionStringProperty;
+	}
+}
